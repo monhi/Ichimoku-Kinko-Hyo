78f19dfe2cf12fce40b5f85ebf9f2dd668897589
Monhi <m.monhi@gmail.com>
2023-12-07 18:07:59 +0330

    showing data

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,21 +1,20 @@
 const express       = require('express');
 const path          = require('path');
-const { join }      = require("path");
+const { join }      = require('path');
 const app           = express();
-const PORT          = 80;
+const PORT          = 3000;
 
 app.use(express.json());
 
-app.set("views", join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-
-const database = require('./db');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-new Router(app,database.db);
+
+app.get("/", (req, res) => {
+    
+});
+
 
 app.listen(PORT,()=>{
 	console.log(`Listening to port ${PORT}`);
